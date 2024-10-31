daffb2fd36bf3ee99b4e75539bbb4431afe2658c
xiaoxuan <yuxx6698@163.com>
2024-10-31 10:29:47 +0800

    feat: 第十五章

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
-import { updateInvoice } from "@/app/lib/actions";
+import { State, updateInvoice } from "@/app/lib/actions";
 
 import {
   CheckIcon,
@@ -11,6 +11,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,10 +20,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
